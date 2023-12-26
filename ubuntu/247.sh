270f8d93c7e247bdee2f3219f4af916ee4e8ceb3
VoxTerrain <154274770+VoxTerrain@users.noreply.github.com>
2023-12-26 21:07:32 +0200

    Update 247.sh
    Version 1.0 of 24/7 Loop (do not change the name!)

--- ubuntu/247.sh
+++ ubuntu/247.sh
@@ -1,1 +1,157 @@
 
+echo "Loop 24/7"
+#!/bin/ksh
+for (( i=1; i>0; i--)); do
+  sleep 1 &
+  printf "  $i \r"
+  wait
+done
+echo "loading."
+#!/bin/ksh
+for (( i=1; i>0; i--)); do
+  sleep 1 &
+  printf "  $i \r"
+  wait
+done
+echo "loading.."
+#!/bin/ksh
+for (( i=1; i>0; i--)); do
+  sleep 1 &
+  printf "  $i \r"
+  wait
+done
+echo "loading..."
+#!/bin/ksh
+for (( i=1; i>0; i--)); do
+  sleep 1 &
+  printf "  $i \r"
+  wait
+done
+echo "loading."
+#!/bin/ksh
+for (( i=1; i>0; i--)); do
+  sleep 1 &
+  printf "  $i \r"
+  wait
+done
+echo "loading.."
+#!/bin/ksh
+for (( i=1; i>0; i--)); do
+  sleep 1 &
+  printf "  $i \r"
+  wait
+done
+echo "loading..."
+#!/bin/ksh
+for (( i=1; i>0; i--)); do
+  sleep 1 &
+  printf "  $i \r"
+  wait
+done
+echo "loading."
+#!/bin/ksh
+for (( i=1; i>0; i--)); do
+  sleep 1 &
+  printf "  $i \r"
+  wait
+done
+echo "loading.."
+#!/bin/ksh
+for (( i=1; i>0; i--)); do
+  sleep 1 &
+  printf "  $i \r"
+  wait
+done
+echo "loading..."
+#!/bin/ksh
+for (( i=1; i>0; i--)); do
+  sleep 1 &
+  printf "  $i \r"
+  wait
+done
+echo "loading."
+#!/bin/ksh
+for (( i=1; i>0; i--)); do
+  sleep 1 &
+  printf "  $i \r"
+  wait
+done
+echo "loading.."
+#!/bin/ksh
+for (( i=1; i>0; i--)); do
+  sleep 1 &
+  printf "  $i \r"
+  wait
+done
+echo "loading..."
+#!/bin/ksh
+for (( i=1; i>0; i--)); do
+  sleep 1 &
+  printf "  $i \r"
+  wait
+done
+echo "loading."
+#!/bin/ksh
+for (( i=1; i>0; i--)); do
+  sleep 1 &
+  printf "  $i \r"
+  wait
+done
+echo "loading.."
+#!/bin/ksh
+for (( i=1; i>0; i--)); do
+  sleep 1 &
+  printf "  $i \r"
+  wait
+done
+echo "loading..."
+#!/bin/ksh
+for (( i=1; i>0; i--)); do
+  sleep 1 &
+  printf "  $i \r"
+  wait
+done
+echo "loading."
+#!/bin/ksh
+for (( i=1; i>0; i--)); do
+  sleep 1 &
+  printf "  $i \r"
+  wait
+done
+echo "loading.."
+#!/bin/ksh
+for (( i=1; i>0; i--)); do
+  sleep 1 &
+  printf "  $i \r"
+  wait
+done
+echo "loading..."
+#!/bin/ksh
+for (( i=1; i>0; i--)); do
+  sleep 1 &
+  printf "  $i \r"
+  wait
+done
+echo "loading."
+#!/bin/ksh
+for (( i=1; i>0; i--)); do
+  sleep 1 &
+  printf "  $i \r"
+  wait
+done
+echo "loading.."
+#!/bin/ksh
+for (( i=1; i>0; i--)); do
+  sleep 1 &
+  printf "  $i \r"
+  wait
+done
+echo "loading..."
+#!/bin/ksh
+for (( i=1; i>0; i--)); do
+  sleep 1 &
+  printf "  $i \r"
+  wait
+done
+echo "Reloading loop"
+bash 247.sh
